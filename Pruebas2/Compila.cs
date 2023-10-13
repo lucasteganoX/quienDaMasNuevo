e06be832272b76dbd87b06a5e10b3d8e24e527ba
sgh4n <sgh4nl5@gmail.com>
2023-10-13 01:42:09 -0300

    Prueba primer commit

--- Pruebas2/Compila.cs
+++ Pruebas2/Compila.cs
@@ -8,6 +8,7 @@ namespace Pruebas
                         // Interfaz_Base_Datos.Get_Atributos_Empleado( "elPePeHunter_Xx" ) ;
                         Console.WriteLine("Hello, World!" +
                                 "Mota mariguana, droga y erecciones");
+                        Console.WriteLine("Tengo la camisa negra, porque negra tengo el alma, yo por ti perdi la calma y casi pierdo hasta mi cama.") ;
                 }
         }
 }
